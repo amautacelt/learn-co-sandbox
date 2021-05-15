68a63fad1b8beb5c2ea02e3eba650ee5e2c4708a
C. L <amautacelt@gmail.com>
2021-05-15 23:10:36 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,23 @@
+def greeting
+  puts "Hello World"
+end
+
+greeting
+greeting
+greeting
+greeting
+greeting
+
+def greeting
+  puts "Hello World"
+end
+
+def say_greeting_five_times
+  greeting
+  greeting
+  greeting
+  greeting
+  greeting
+end
+
+say_greeting_five_times
